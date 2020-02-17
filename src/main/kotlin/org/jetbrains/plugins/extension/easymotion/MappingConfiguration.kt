fc772291e2e585f40147db71ca6f2006b85365dc
Alex Plate <aleksei.plate@jetbrains.com>
2020-02-17 09:49:25 +0300

    Configure ace mappings later

--- src/main/kotlin/org/jetbrains/plugins/extension/easymotion/MappingConfiguration.kt
+++ src/main/kotlin/org/jetbrains/plugins/extension/easymotion/MappingConfiguration.kt
@@ -20,6 +20,7 @@ package org.jetbrains.plugins.extension.easymotion
 
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.KeyboardShortcut
+import com.intellij.openapi.application.ApplicationManager
 import com.maddyhome.idea.vim.VimPlugin
 import com.maddyhome.idea.vim.command.MappingMode
 import com.maddyhome.idea.vim.extension.VimExtensionFacade
@@ -36,20 +37,22 @@ object MappingConfigurator {
     )
 
     fun configureMappings() {
-        for ((actionId, alternative) in aceJumpAlternatives) {
-            val action = ActionManager.getInstance().getAction(actionId) ?: continue
-            val shortcuts = action.shortcutSet.shortcuts
-            for (shortcut in shortcuts) {
-                if (!shortcut.isKeyboard || shortcut !is KeyboardShortcut) continue
-                val keyStroke = shortcut.firstKeyStroke
+        ApplicationManager.getApplication().invokeLater {
+            for ((actionId, alternative) in aceJumpAlternatives) {
+                val action = ActionManager.getInstance().getAction(actionId) ?: continue
+                val shortcuts = action.shortcutSet.shortcuts
+                for (shortcut in shortcuts) {
+                    if (!shortcut.isKeyboard || shortcut !is KeyboardShortcut) continue
+                    val keyStroke = shortcut.firstKeyStroke
 
-                VimExtensionFacade.putKeyMapping(
-                    MappingMode.NVO,
-                    listOf(keyStroke),
-                    StringHelper.parseKeys(command(alternative)),
-                    true
-                )
-                VimPlugin.getKey().savedShortcutConflicts[keyStroke] = ShortcutOwner.VIM
+                    VimExtensionFacade.putKeyMapping(
+                        MappingMode.NVO,
+                        listOf(keyStroke),
+                        StringHelper.parseKeys(command(alternative)),
+                        true
+                    )
+                    VimPlugin.getKey().savedShortcutConflicts[keyStroke] = ShortcutOwner.VIM
+                }
             }
         }
     }
