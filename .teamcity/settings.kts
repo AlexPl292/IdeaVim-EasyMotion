4216059991a8e33c3c7fe5f1c8e57c7a0c875865
aleksei.plate <aleksei.plate@TeamCity>
2021-02-15 07:34:47 +0000

    TeamCity change in 'IntelliJ IDEA plugins / IdeaVim / IdeaVim-EasyMotion' project: runners of 'Build Master' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -88,7 +88,9 @@ object BuildMaster : BuildType({
                 git clone https://github.com/JetBrains/ideavim IdeaVIM
                 
                 echo "Apply patch"
-                git apply -- masters.patch
+                sed -i '.gradle' "s/id 'org.jetbrains.intellij'.*/id 'org.jetbrains.intellij'/g" ./IdeaVIM/build.gradle
+                sed -i '.gradle' 's/kotlin("jvm").*/kotlin("jvm")/g' ./AceJump/build.gradle.kts
+                sed -i '.gradle' 's/id("org.jetbrains.intellij").*/id("org.jetbrains.intellij")/g' ./AceJump/build.gradle.kts
             """.trimIndent()
         }
         gradle {
